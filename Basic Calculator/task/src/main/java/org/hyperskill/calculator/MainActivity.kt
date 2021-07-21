ebbff896068583b2cd67374164b6a60eb2c100c0
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-07-20 23:47:42 -0400

    Stage 2 of 4 for JetBrains Academy - Kotlin Android - Basic Calculator project.
    Description
    
    Once we have the layout, let's continue with our input. So far, the buttons are plain pictures. They don't seem to work. Infuse life into them by adding operations and showing the numbers you've typed on the screen.
    Objectives
    
    Power up the buttons you have created in Stage 1.
    
    1. After clicking on any button, a corresponding number should be added to EditText. Make sure you do not overwrite the number you've typed in earlier.
    2. Users are not allowed to type multiple 0 when EditText contains a single 0. The same thing should apply to the dot button (only one dot can be displayed).
    3. Make the Clear button set EditText back to 0.
    
    MainActivity.kt changes:
    • added binding to make adding buttons to it easier
    • created viewModel for communicating with CalculatorViewModel
    • used viewModel to observe changes made to result in CalculatorViewModel through getResult.
    • added listener to call viewModel.digitPressed for buttons 1 through 9
    • button0 - added a listener that directs its button pressed to viewModel.zeroPressed()
    • dotButton - added a listener that directs its button pressed to viewModel.dotPressed()
    • clearButton - added a listener that directs its button pressed to viewModel.clearPressed()
    • setListener() - added to make adding buttons 1 through 9 to the listener easier. It receives a button and the listener to apply it to.

--- Basic Calculator/task/src/main/java/org/hyperskill/calculator/MainActivity.kt
+++ Basic Calculator/task/src/main/java/org/hyperskill/calculator/MainActivity.kt
@@ -1,11 +1,40 @@
 package org.hyperskill.calculator
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import org.hyperskill.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val viewModel: CalculatorViewModel by viewModels()
+        val listener = View.OnClickListener { v -> viewModel.digitPressed((v as Button).text.toString()) }
+
+        viewModel.getResult.observe(this, { result -> binding.editText.setText(result) })
+
+        setListener(binding.button1, listener)
+        setListener(binding.button2, listener)
+        setListener(binding.button3, listener)
+        setListener(binding.button4, listener)
+        setListener(binding.button5, listener)
+        setListener(binding.button6, listener)
+        setListener(binding.button7, listener)
+        setListener(binding.button8, listener)
+        setListener(binding.button9, listener)
+
+        binding.button0.setOnClickListener { viewModel.zeroPressed() }
+        binding.dotButton.setOnClickListener { viewModel.dotPressed() }
+        binding.clearButton.setOnClickListener { viewModel.clearPressed() }
     }
+
+    private fun setListener(button: Button, listener: View.OnClickListener) = button.setOnClickListener(listener)
 }
